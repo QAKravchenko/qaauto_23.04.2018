ef8d109437284635aa59d682cd213a1870383bb1
Svyatoslav Kravchenko <skravchenko@adyax.com>
2018-05-14 17:33:25 +0300

    Created negative test cases for 'Login' form

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -19,7 +19,10 @@ public class LinkedinLoginTest
 
         webDriver.get("https://www.linkedin.com/");
 
-        String actualPageTitle = webDriver.getTitle();
+//        String actualPageTitle = webDriver.getTitle();
+        WebElement loginField = webDriver.findElement(By.id("login-email"));
+        WebElement passwordField = webDriver.findElement(By.id("login-password"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
 
 // Checking the title of the page is "LinkedIn: Log In or Sign Up"
         Assert.assertEquals(webDriver.getTitle(),
@@ -32,20 +35,69 @@ public class LinkedinLoginTest
                 "Homepage URL is wrong!");
 
 // Checking "Login" field is displayed on "Login" page
-        WebElement loginField = webDriver.findElement(By.id("login-email"));
         Assert.assertTrue(loginField.isDisplayed(), "Login field isn't displayed!");
 
+// NEGATIVE TEST CASES FOR LOGIN
+     // 1 case
+        signInButton.click();
+        Assert.assertNotEquals(webDriver.getCurrentUrl(),
+                "https://www.linkedin.com/feed/",
+                "Login successful !!!");
 
+     // 2 case
+        loginField.sendKeys("skravchenko@adyax.com");
+        signInButton.click();
+        Assert.assertNotEquals(webDriver.getCurrentUrl(),
+                "https://www.linkedin.com/feed/",
+                "Login successful !!!");
+
+     // 3 case
+        loginField.clear();
+        passwordField.sendKeys("adyax111");
+        signInButton.click();
+        Assert.assertNotEquals(webDriver.getCurrentUrl(),
+                "https://www.linkedin.com/feed/",
+                "Login successful !!!");
+
+     // 4 case
+        loginField.clear();
+        passwordField.clear();
+        loginField.sendKeys("skravchenko@adyax.com");
+        passwordField.sendKeys("adyax11");
+        signInButton.click();
+
+        sleep(3000);
+
+        String errorMessage = webDriver.findElement(By.xpath("//strong")).getText();
+        Assert.assertNotEquals(webDriver.getCurrentUrl(),
+                "https://www.linkedin.com/feed/",
+                "Login successful !!!");
+        Assert.assertTrue(errorMessage.contentEquals("There were one or more errors in your submission. Please correct the marked fields below."));
+
+     // 5 case
+        WebElement loginField2 = webDriver.findElement(By.id("session_key-login"));
+        WebElement passwordField2 = webDriver.findElement(By.id("session_password-login"));
+        WebElement signInButton2 = webDriver.findElement(By.id("btn-primary"));
+        loginField2.clear();
+        passwordField2.clear();
+        loginField2.sendKeys("skravchenkoadyax.com");
+        passwordField2.sendKeys("adyax111");
+        signInButton2.click();
+        Assert.assertNotEquals(webDriver.getCurrentUrl(),
+                "https://www.linkedin.com/feed/",
+                "Login successful !!!");
+
+        sleep(3000);
+
+/*
 // START CHECKING SUCCESSFUL LOGIN
 // Typing login into "Login" field
         loginField.sendKeys("skravchenko@adyax.com");
 
 // Typing password into "Password" field
-        WebElement passwordField = webDriver.findElement(By.id("login-password"));
-        passwordField.sendKeys("testerov");
+        passwordField.sendKeys("tomplier");
 
 // Clicking on "Submit" button
-        WebElement loginButton = webDriver.findElement(By.id("login-submit"));
         loginButton.click();
 
         sleep(3000);
@@ -72,7 +124,7 @@ public class LinkedinLoginTest
 // Checking "Home" button is displayed on Homepage
         WebElement homeButton = webDriver.findElement(By.id("feed-nav-item"));
         Assert.assertTrue(homeButton.isDisplayed(), "Home button isn't displayed!");
-
+*/
         webDriver.close();
 
     }
